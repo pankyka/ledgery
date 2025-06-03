7945056aa313120ccf5fa0c3a18351dfd1c9bdf6
Gabor Pankotay <mrpanky@gmail.com>
2025-06-03 14:27:54 +0200

    graphql+apollo sandbox

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
-  "users-permissions": {
+export default ({env}) => ({
+  'users-permissions': {
     config: {
-      jwtSecret: env("JWT_SECRET")
+      jwtSecret: env('JWT_SECRET'),
       // providers: {
       //   google: {
       //     clientId: process.env.GOOGLE_CLIENT_ID,
@@ -14,6 +14,22 @@ export default ({ env }) => ({
       //     redirectUri: "http://localhost:1337/api/connect/microsoft/callback",
       //   },
       // },
+    },
+  },
+  graphql: {
+    enabled: true,
+    config: {
+      // set this to true if you want to enable the playground in production
+      playgroundAlways: true,
+    },
+  },
+  'apollo-sandbox': {
+    // enables the plugin only in development mode
+    // if you also want to use it in production, set this to true
+    // keep in mind that graphql playground has to be enabled
+    enabled: true, // process.env.NODE_ENV === 'production' ? false : true,
+    config: {
+      // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
     },
   },
 });
