378bf5a36becb08124a37e80fe385da8209ad2a0
Gabor Pankotay <mrpanky@gmail.com>
2025-06-03 15:04:28 +0200

    libs

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -23,13 +23,4 @@ export default ({env}) => ({
       playgroundAlways: true,
     },
   },
-  'apollo-sandbox': {
-    // enables the plugin only in development mode
-    // if you also want to use it in production, set this to true
-    // keep in mind that graphql playground has to be enabled
-    enabled: true, // process.env.NODE_ENV === 'production' ? false : true,
-    config: {
-      // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
-    },
-  },
 });
