d806ca372afadc45e648a0a027a67190c8583a42
Gabor Pankotay <mrpanky@gmail.com>
2025-06-04 13:06:14 +0200

    Update strapi-server.ts

--- apps/cms/src/extensions/users-permissions/strapi-server.ts
+++ apps/cms/src/extensions/users-permissions/strapi-server.ts
@@ -3,10 +3,36 @@ import registerController from './controllers/register';
 export default plugin => {
   console.log('🔁 Registering custom controller: registerWithTenant');
 
-  plugin.controllers.user = {
-    ...plugin.controllers.user,
-    ...registerController,
+  // plugin.controllers.user = {
+  //   ...plugin.controllers.user,
+  //   ...registerController,
+  // };
+
+  // Capture the original factory function for the `auth` controller
+  const originalAuthFactory = plugin.controllers.auth;
+
+  // Create a new factory function that extends the original
+  plugin.controllers.auth = ({strapi}) => {
+    const originalAuth = originalAuthFactory({strapi});
+
+    // Add a custom function to the `auth` controller
+    originalAuth.registerWithTenant = registerController.registerWithTenant;
+
+    return originalAuth;
   };
+
+  // Extend the plugin routes if needed
+  plugin.routes['content-api'].routes.push({
+    method: 'POST',
+    path: '/auth/register-with-tenant',
+    handler: 'auth.registerWithTenant',
+    config: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+      type: 'content-api',
+    },
+  });
 
   setTimeout(() => {
     const routes = strapi.server.router.stack
