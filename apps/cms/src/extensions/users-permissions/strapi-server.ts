7e772281064562ccb2f47398aa1ddbe915a6dff4
Gabor Pankotay <mrpanky@gmail.com>
2025-06-04 12:48:45 +0200

    Update strapi-server.ts

--- apps/cms/src/extensions/users-permissions/strapi-server.ts
+++ apps/cms/src/extensions/users-permissions/strapi-server.ts
@@ -8,5 +8,17 @@ export default plugin => {
     ...registerController,
   };
 
+  setTimeout(() => {
+    const routes = strapi.server.router.stack
+      .filter(layer => layer?.path && layer?.methods?.length)
+      .map(layer => ({
+        method: layer.methods.join(','),
+        path: layer.path,
+      }));
+
+    console.log('📡 Regisztrált útvonalak:');
+    console.table(routes);
+  }, 500); // kell egy kis késleltetés, amíg router kész
+
   return plugin;
 };
