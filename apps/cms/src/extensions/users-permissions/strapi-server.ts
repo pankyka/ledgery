708a2198e5d42c8649bcab4be812fd7f6f29b3a7
Gabor Pankotay <mrpanky@gmail.com>
2025-06-25 14:19:39 +0200

    feat: log sign in activity

--- apps/cms/src/extensions/users-permissions/strapi-server.ts
+++ apps/cms/src/extensions/users-permissions/strapi-server.ts
@@ -8,6 +8,36 @@ export default plugin => {
 
     originalAuth.registerWithTenant = registerWithTenant;
 
+    const authLoginFn =
+      typeof originalAuth.login === 'function'
+        ? 'login'
+        : typeof originalAuth.callback === 'function'
+          ? 'callback'
+          : null;
+
+    if (authLoginFn) {
+      const defaultFn = originalAuth[authLoginFn];
+
+      originalAuth[authLoginFn] = async ctx => {
+        const result = await defaultFn(ctx);
+
+        try {
+          const user = ctx.state?.user;
+          const tenant = user?.tenant;
+
+          if (user && tenant) {
+            await strapi
+              .service('api::tenant-activity.tenant-activity')
+              .log(user, tenant, 'SIGN_IN', ctx.request.ip);
+          }
+        } catch (err) {
+          strapi.log.error('Failed to log sign in activity', err);
+        }
+
+        return result;
+      };
+    }
+
     return originalAuth;
   };
 
