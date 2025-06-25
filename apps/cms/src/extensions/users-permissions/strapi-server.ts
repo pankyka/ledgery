2eb3e38af7033da417d7518ddc498fffcb72036d
Gabor Pankotay <mrpanky@gmail.com>
2025-06-25 16:01:04 +0200

    Update strapi-server.ts

--- apps/cms/src/extensions/users-permissions/strapi-server.ts
+++ apps/cms/src/extensions/users-permissions/strapi-server.ts
@@ -1,6 +1,16 @@
 import registerWithTenant from './controllers/register-with-tenant';
 
 export default plugin => {
+  const originalAuthFactory = plugin.controllers.auth;
+
+  plugin.controllers.auth = ({ strapi }) => {
+    const originalAuth = originalAuthFactory({ strapi });
+
+    originalAuth.registerWithTenant = registerWithTenant;
+
+    return originalAuth;
+  };
+
   plugin.routes['content-api'].routes.push({
     method: 'POST',
     path: '/auth/local/register-with-tenant',
