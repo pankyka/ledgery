e60de817a8afbe57a95a84952c27a605c719d6de
Gabor Pankotay <mrpanky@gmail.com>
2025-07-03 15:13:19 +0200

    Update invoice-item.ts

--- apps/cms/src/api/invoice-item/controllers/invoice-item.ts
+++ apps/cms/src/api/invoice-item/controllers/invoice-item.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::invoice-item.invoice.item');
+export default factories.createCoreController('api::invoice-item.invoice-item');
