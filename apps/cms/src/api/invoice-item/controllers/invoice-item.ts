5e084f823d5ed35dbc121c1c8b84672b5b7e6665
Gabor Pankotay <mrpanky@gmail.com>
2025-07-03 14:29:10 +0200

    content-type routes and controllers

--- apps/cms/src/api/invoice-item/controllers/invoice-item.ts
+++ apps/cms/src/api/invoice-item/controllers/invoice-item.ts
@@ -1,1 +1,3 @@
-export default {};
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::invoice-item.invoice.item');
--- apps/cms/src/api/tenant-activity/controllers/tenant-activity.ts
+++ apps/cms/src/api/tenant-activity/controllers/tenant-activity.ts
@@ -1,1 +1,3 @@
-export default {};
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::tenant-activity.tenant-activity');
